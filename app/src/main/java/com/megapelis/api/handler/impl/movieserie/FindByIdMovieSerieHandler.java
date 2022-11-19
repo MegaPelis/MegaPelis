75a67d0daff5b2773223167e7a948f801d012ce4
Sergio Buitrago <ssbarrio@bancolombia.com.co>
2022-11-19 14:43:56 -0500

    feat:Fabrica consultar las peliculas e series.

---
+++ app/src/main/java/com/megapelis/api/handler/impl/movieserie/FindByIdMovieSerieHandler.java
@@ -0,0 +1,19 @@
+package com.megapelis.api.handler.impl.movieserie;
+
+import com.megapelis.api.handler.APIController;
+import com.megapelis.api.model.dto.request.generic.Request;
+import com.megapelis.api.model.dto.response.body.movieserie.FindByIdMovieSerieRS;
+import com.megapelis.api.model.http.HTTP;
+
+public class FindByIdMovieSerieHandler extends APIController<FindByIdMovieSerieRS> {
+
+    @Override
+    public Request buildRequest(Object object) {
+        return null;
+    }
+
+    @Override
+    public HTTP<FindByIdMovieSerieRS> buildHttp() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/megapelis/api/handler/impl/movieserie/UpdateStatusMovieSerieHandler.java
@@ -0,0 +1,17 @@
+package com.megapelis.api.handler.impl.movieserie;
+
+import com.megapelis.api.handler.APIController;
+import com.megapelis.api.model.dto.request.generic.Request;
+import com.megapelis.api.model.http.HTTP;
+
+public class UpdateStatusMovieSerieHandler extends APIController<Object> {
+    @Override
+    public Request buildRequest(Object object) {
+        return null;
+    }
+
+    @Override
+    public HTTP<Object> buildHttp() {
+        return null;
+    }
+}
