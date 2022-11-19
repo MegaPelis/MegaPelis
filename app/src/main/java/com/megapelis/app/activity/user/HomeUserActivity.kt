5ae98c569833fa708cc3b90850fd3308373a0c27
Sergio Buitrago <ssbarrio@bancolombia.com.co>
2022-11-19 18:23:21 -0500

    feat:Card de peliculas y serie.

---
+++ app/src/main/java/com/megapelis/app/activity/user/HomeUserActivity.kt
@@ -0,0 +1,12 @@
+package com.megapelis.app.activity.user
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.megapelis.R
+
+class HomeUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_user)
+    }
+}
