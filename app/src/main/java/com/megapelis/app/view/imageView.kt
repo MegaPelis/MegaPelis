faf7149e9f9678b76c7f048624c6e9ac92b59115
Yadir - Garcia <yadir.garcia@pragma.com.co>
2022-11-30 05:49:13 -0500

    Lambda funcionando

--- app/src/main/java/com/megapelis/app/view/imageView.kt
+++ app/src/main/java/com/megapelis/app/view/imageView.kt
@@ -1,20 +1,27 @@
 package com.megapelis.app.view
 
 import androidx.fragment.app.Fragment
+import com.megapelis.api.factory.ImageFactory
 import com.megapelis.api.factory.MovieSerieFactory
+import com.megapelis.api.model.dto.request.body.image.FindAllImageRQ
 import com.megapelis.api.model.dto.response.body.image.FindAllImageRS
 import com.megapelis.api.model.dto.response.generic.Response
+import com.megapelis.api.model.enums.operation.ImageOperationEnum
 import com.megapelis.api.model.enums.operation.MegaPelisTypeServiceEnum
 import com.megapelis.api.model.enums.operation.MovieSerieOperationEnum
 import com.megapelis.api.model.factory.DataFactory
+import com.megapelis.api.util.APICommon
 
-class ImageView : Fragment(){
+class ImageView : Fragment() {
 
-//    fun execute(): FindAllImageRS {
-//        val findAllImageRS: FindAllImageRS
-//        val dataFactory : DataFactory<FindAllImageRS> = DataFactory(null, null, FindAllImageRS::class.java)
-//        val response : Response =  MovieSerieFactory.handler(dataFactory, MovieSerieOperationEnum.FIND_ALL)
-//        findAllImageRS = response.data as FindAllImageRS;
-//        return findAllImageRS
-//    }
+    fun execute(): FindAllImageRS {
+        val findAllImageRQ = FindAllImageRQ()
+        val findAllImageRS: FindAllImageRS
+        val dataFactory: DataFactory<FindAllImageRS> =
+            DataFactory(MegaPelisTypeServiceEnum.IMAGE, findAllImageRQ, FindAllImageRS::class.java)
+        val response: Response = ImageFactory.handler(dataFactory, ImageOperationEnum.FIND_ALL)
+        APICommon.output(response)
+        findAllImageRS = response.data as FindAllImageRS;
+        return findAllImageRS
+    }
 }
